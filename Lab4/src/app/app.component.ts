304b1785a5fe5d76b721904f4029e5eb01288f89
aybek <157131030+um1tt@users.noreply.github.com>
2025-02-16 20:08:28 +0500

    Add files via upload

---
+++ Lab4/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ProductsComponent } from './components/products/products.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ProductsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'kaspi-shop';
+}
